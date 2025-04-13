2dbe833648cc1aadf8940e681db753db5e5c7832
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-13 19:27:35 +0200

    Database and migrations!

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,34 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gonstoll/workouts/internal/api"
+	"github.com/gonstoll/workouts/internal/store"
+	"github.com/gonstoll/workouts/migrations"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	workoutHander := api.NewWorkoutHandler()
@@ -22,6 +36,7 @@ func NewApplication() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHander,
+		DB:             pgDB,
 	}
 
 	return app, nil
