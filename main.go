c05178ad0a1948a3c4bf100c863c5734333e8942
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-13 13:45:49 +0200

    Setting up server

--- main.go
+++ main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gonstoll/workouts/internal/app"
+)
+
 func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("App is running...")
+
+	server := http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	http.HandleFunc("/health", HealthCheck)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Status is available\n")
 }
