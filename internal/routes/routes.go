3475af052ccb2edc32679f6f99185c05a7b0d2d7
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-18 13:57:42 +0200

    Route for registering user

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,10 +7,16 @@ import (
 
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
+	// Health
 	r.Get("/health", app.HealthCheck)
+
+	// Workouts
 	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
 	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
 	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
 	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
+
+	// Users
+	r.Post("/users", app.UserHandler.HandleRegisterUser)
 	return r
 }
